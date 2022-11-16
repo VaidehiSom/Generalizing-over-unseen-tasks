5f3f604e2e10cdd90686f7792176af0f557bb4ae
VaidehiSom <som.vaidehi920@gmail.com>
2022-11-16 17:12:19 -0500

    updating recording ways

--- src/data_collection/scripts/data_collection.py
+++ src/data_collection/scripts/data_collection.py
@@ -15,16 +15,33 @@ class DataCollection:
         self.camera_feed_depth_sub = rospy.Subscriber('/camera/depth/image_raw', Image, self.camera_depth_feed_cb)
         # self.file = open("data_collection.txt", "a")
         # self.bag = rosbag.Bag("data_collection.bag", 'a')
-        self.f = open("/home/vaidehi/Downloads/Kinova_RoboticArm/src/data_collection/data_collection.csv", 'w')
-        self.writer = csv.writer(self.f)
+        self.joint_state_f = open("/home/vaidehi/Downloads/Kinova_RoboticArm/src/data_collection/joint_state.csv", 'w')
+        self.joint_state_writer = csv.writer(self.joint_state_f)
+        self.camera_color_f = open("/home/vaidehi/Downloads/Kinova_RoboticArm/src/data_collection/camera_color_feed.csv", 'w')
+        self.camera_color_writer = csv.writer(self.camera_color_f)
+        self.camera_depth_f = open("/home/vaidehi/Downloads/Kinova_RoboticArm/src/data_collection/camera_depth_feed.csv", 'w')
+        self.camera_depth_writer = csv.writer(self.camera_depth_f)
+        
+        self.joint_state_f_2 = open("/home/vaidehi/Downloads/Kinova_RoboticArm/src/data_collection/joint_state_2.csv", 'w')
+        self.joint_state_writer_2 = csv.writer(self.joint_state_f_2)
+        self.camera_color_f_2 = open("/home/vaidehi/Downloads/Kinova_RoboticArm/src/data_collection/camera_color_feed_2.csv", 'w')
+        self.camera_color_writer_2 = csv.writer(self.camera_color_f_2)
+        self.camera_depth_f_2 = open("/home/vaidehi/Downloads/Kinova_RoboticArm/src/data_collection/camera_depth_feed_2.csv", 'w')
+        self.camera_depth_writer_2 = csv.writer(self.camera_depth_f_2)
+        
         print("DataCollection")
 
     def __del__(self):
         # self.file.close()
-        self.f.close()
+        self.joint_state_f.close()
+        self.camera_color_f.close()
+        self.camera_depth_f.close()
+        self.joint_state_f_2.close()
+        self.camera_color_f_2.close()
+        self.camera_depth_f_2.close()
         print("~DataCollection")
 
-    def joint_states_cb(self, data):
+    def joint_states_cb(self, msg):
         """header: 
             seq: 1774
             stamp: 
@@ -37,19 +54,24 @@ class DataCollection:
           velocity: [-0.002289906319662698, 0.17106830242405116, 0.010021302492616237, 0.3409748008225553, 0.0042357197429195486, -0.1534153726908928, -0.00755036961837568, 0.0, 0.0, -0.0, 0.0, 0.0, -0.0]
           effort: [-0.31865596771240234, 17.228782653808594, 0.03316592797636986, -10.732674598693848, -0.0162641778588295, -2.255537509918213, -0.31152278184890747, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         """
-        self.end_effector_pos = data
+        self.end_effector_pos_header = msg.header
+        self.end_effector_pos_velocity = msg.velocity
+        self.end_effector_pos = msg
+        self.joint_state_writer.writerow(self.end_effector_pos_header)
+        self.joint_state_writer.writerow(self.end_effector_pos_velocity)
     
-    def camera_depth_feed_cb(self, data):
-        self.camera_depth_feed = data
+    def camera_depth_feed_cb(self, msg):
+        self.camera_depth_feed = msg
+        self.camera_depth_writer.writerow(self.camera_depth_feed)
 
-    def camera_color_feed_cb(self, data):
-        self.camera_color_feed = data
+    def camera_color_feed_cb(self, msg):
+        self.camera_color_feed = msg
+        self.camera_color_writer.writerow(self.camera_color_feed)
 
     def record_data(self):
-        self.writer.writerow(self.end_effector_pos)
-        # self.bag.write('/my_gen3/joint_states', self.end_effector_pos)
-        # self.bag.write('', self.camera_feed)
-
+        self.joint_state_writer_2.writerow(self.end_effector_pos)
+        self.camera_depth_writer_2.writerow(self.camera_depth_feed)
+        self.camera_color_writer_2.writerow(self.camera_color_feed)
 
 
 if __name__ == '__main__':
@@ -61,9 +83,12 @@ if __name__ == '__main__':
 
     for i in range(int(a)):
         now = rospy.get_rostime()
-        while(rospy.Time.now() != now + rospy.Duration(timer_for_one_round_mins*60)):
-            data_c.record_data()
-            rospy.sleep(0.01)
+        b = input("Start round %f?", i)
+        if(b=='Y' or b=='y'):
+            while(rospy.Time.now() != now + rospy.Duration(timer_for_one_round_mins*60)):
+                data_c.record_data()
+                rospy.sleep(0.01)
+        print("round finish")
         
         # ans = input("Save this data?")
         # if(ans=="y" or ans=="Y"):
